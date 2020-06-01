0762cc5fb6ef901865da06d2a0121f125994aceb
Calvin Opiti <opiticalvin@gmail.com>
2020-06-01 22:46:01 +0200

    added a database

--- solutions/main.py
+++ solutions/main.py
@@ -1,9 +1,14 @@
-from the_app import create_app
+from the_app import create_app, db
 from flask_script import Manager, Shell, Server
+from flask_migrate import Migrate, MigrateCommand
+
+from apis.contact.models import MessageModel
 
 app = create_app()
+migrate = Migrate(app,db, compare_type=True)
 manager = Manager(app)
 
+manager.add_command("db", MigrateCommand)
 manager.add_command("runserver", Server(host="0.0.0.0", port=3000, threaded=True))
 
 if __name__ == '__main__':
