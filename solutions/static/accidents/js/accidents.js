5f0b1da4c315a483f58fb54c0fda5cc2a6d95106
Calvin Opiti <opiticalvin@gmail.com>
2020-06-28 02:44:34 +0200

    fetch query used to update accidents layer, and for charting

--- solutions/static/accidents/js/accidents.js
+++ solutions/static/accidents/js/accidents.js
@@ -11,23 +11,22 @@ var county = new ol.layer.Vector({
 });
 // heatmap section 
 let heatmapLayer = new ol.layer.Heatmap({
-  // source: new VectorSource(),
   source: new ol.source.Vector(),
   radius: 8,
-  // shadow: 500,
   blur:15
 });
-// end of heatmap sectipn
+// end of heatmap section
 
-var accident_source = new ol.source.Vector({
-    url: '/accidents/accidentsGeoJSON/?county=null',
-    format: new ol.format.GeoJSON()
-  });
+// var accident_source = new ol.source.Vector({
+//     // url: '/accidents/accidentsGeoJSON/?county=null',
+//     format: new ol.format.GeoJSON()
+// });
 
-var accident = new ol.layer.Vector({
-  // visible: false,
-  source: accident_source
-});
+// window.accident = new ol.layer.Vector({
+//   // visible: false,
+//   source: accident_source
+// });
+window.accident = new ol.layer.Vector();
 
 var map = new ol.Map({
   layers: [raster, county, accident, heatmapLayer],
@@ -46,124 +45,129 @@ var select = new ol.interaction.Select({
 
 map.addInteraction(select);
 
-select.on('select', function(e) {
-  $("#spinner").prop('hidden', false);
-  // console.log(e.target.getFeatures().getArray()[0].get('name'))
+select.on('select', function(e) {  
   // accident.setSource({
   //   url: '/accidents/accidentsGeoJSON/?county='+e.target.getFeatures().getArray()[0].get('name'),
   //   format: new ol.format.GeoJSON()
   // });
-  
-  let extent = e.target.getFeatures().getArray()[0].getGeometry().getExtent();
-  map.getView().fit(extent);
-  accident.getSource().setUrl('/accidents/accidentsGeoJSON/?county='+e.target.getFeatures().getArray()[0].get('name'));
-  accident.getSource().refresh();
-  fetch('/accidents/accidentsStatistics/?county='+e.target.getFeatures().getArray()[0].get('name'))
-  .then(response =>{
-    if (!response.ok){
-      throw new Error(response.json());
-    }
-    return response.json();
-  })
-  .then(responseData => {
-    $("#spinner").prop('hidden', true);
-    flash("Query for accident data succcessful",{
-      'bgColor': "#51B155"
-    });
-    // heatmap section
-    // heatmapLayer.getSource().clear();
-    let featList = accident.getSource().getFeatures();  //  this.source  get the target features 
-    // console.log(featList[0]);
-    for(let item of featList ) {              //  copy the feature to the heatmap layer
-          let geometry = item.get('geometry');
-          let coordinate = geometry.flatCoordinates;
-          let feature = new ol.Feature({
-              geometry: new ol.geom.Point(coordinate),
-          })
-          heatmapLayer.getSource().addFeature(feature);
-    }
-    // heatmapLayer.getSource().refresh();
-    // this.map.addLayer(heatmapLayer);  //  add heatmap layer to map
 
-    // end of heatmap section
+  if (e.target.getFeatures().getArray().length > 0){
+    $("#spinner").prop('hidden', false);
 
-    // graph function will go here
-    let dataLabels = []
-    let no_of_accidents = []
-    let no_of_casualties = []
-    let no_of_vehicles = []
+    let extent = e.target.getFeatures().getArray()[0].getGeometry().getExtent();
+    map.getView().fit(extent);
 
-    for (level in responseData){
-      dataLabels.push("Level "+level);
-      no_of_accidents.push(responseData[level]["accident_count"])
-      no_of_casualties.push(responseData[level]['casualty_total'])
-      no_of_vehicles.push(responseData[level]["vehicle_total"])
-    }
+    fetch('/accidents/accidentsGeoJSON/?county='+e.target.getFeatures().getArray()[0].get('name'))
+    .then(response =>{
+      if (!response.ok){
+        throw new Error(response.json());
+      }
+      return response.json();
+    })
+    .then(response => {
+      $("#spinner").prop('hidden', true);
+      if (response["status_code"] === 200){
+        flash(response['message'],{
+          'bgColor': "#51B155"
+        });
 
-    $('#myChart').empty();
-    var ctx = document.getElementById('myChart').getContext('2d');
-		var severityData = {
-      // labels: ["Level 1", "Level 2", "Level 3"],
-      labels: dataLabels,
-			datasets: [
-				{
-					label: "No of Accidents",
-					backgroundColor: "rgba(54, 162, 235, 0.2)",
-					borderColor: "rgba(54, 162, 235, 1)",
-					borderWidth: 1,
-          // data: [29,331,1384]
-          data: no_of_accidents
-				},
-				{
-					label: "No of Casulties",
-					backgroundColor: "rgba(255, 99, 132, 0.2)",
-					borderColor: "rgba(255, 99, 132, 1)",
-					borderWidth: 1,
-          // data: [47,461,1809]
-          data: no_of_casualties
-				},
-				{
-					label: "No of Vehicles",
-					backgroundColor: "rgba(255, 206, 86, 0.2)",
-					borderColor: "rgba(255, 206, 86, 1)",
-					borderWidth: 1,
-          // data: [43,599,2616]
-          data: no_of_vehicles
-				}
-			]
-		};
-		
-		var myChart = new Chart(ctx, {
-			type: 'bar',
-			data: severityData,
-			options: {
-				barValueSpacing: 20,
-				scales: {
-					yAxes: [{
-						ticks: {
-							beginAtZero: true
-						}
-					}]
-        },
-        title: {
-          display: true,
-          text: 'Statistics by Accident Severity: County='+e.target.getFeatures().getArray()[0].get('name')
+        let geojs_source = new ol.source.Vector({
+          features: (new ol.format.GeoJSON()).readFeatures(response["geojson_data"],{ 
+            dataProjection: 'EPSG:4326',
+            featureProjection:'EPSG:3857' })
+        });
+        
+        heatmapLayer.getSource().clear();
+        accident.setSource(geojs_source);
+
+        // heatmap section
+        // heatmapLayer.getSource().clear();
+        let featList = accident.getSource().getFeatures();  //  this.source  get the target features 
+        // console.log(featList[0]);
+        for(let item of featList ) {              //  copy the feature to the heatmap layer
+              let geometry = item.get('geometry');
+              let coordinate = geometry.flatCoordinates;
+              let feature = new ol.Feature({
+                  geometry: new ol.geom.Point(coordinate),
+              })
+              heatmapLayer.getSource().addFeature(feature);
         }
-			}
-		});
-    
-  })
-  .catch(error => {
-    $("#spinner").prop('hidden', true);
-    flash(error,{
-      'bgColor': "#E62229"
-    });
-    
-  })
+        // end of heatmap section
 
-  // if (accident.getSource().getState() === 'ready'){
-  //   console.log(accident.getSource().getState());
-  //   $("#spinner").prop('hidden', true);
-  // }
-  
+        // graph function will go here
+        let dataLabels = []
+        let no_of_accidents = []
+        let no_of_casualties = []
+        let no_of_vehicles = []
+
+        let accidentStatistics = response["stats_data"];
+
+        for (level in accidentStatistics){
+          dataLabels.push("Level "+level);
+          no_of_accidents.push(accidentStatistics[level]["accident_count"])
+          no_of_casualties.push(accidentStatistics[level]['casualty_total'])
+          no_of_vehicles.push(accidentStatistics[level]["vehicle_total"])
+        }
+
+        $('#myChart').empty();
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var severityData = {
+          // labels: ["Level 1", "Level 2", "Level 3"],
+          labels: dataLabels,
+          datasets: [
+            {
+              label: "No of Accidents",
+              backgroundColor: "rgba(54, 162, 235, 0.2)",
+              borderColor: "rgba(54, 162, 235, 1)",
+              borderWidth: 1,
+              // data: [29,331,1384]
+              data: no_of_accidents
+            },
+            {
+              label: "No of Casulties",
+              backgroundColor: "rgba(255, 99, 132, 0.2)",
+              borderColor: "rgba(255, 99, 132, 1)",
+              borderWidth: 1,
+              // data: [47,461,1809]
+              data: no_of_casualties
+            },
+            {
+              label: "No of Vehicles",
+              backgroundColor: "rgba(255, 206, 86, 0.2)",
+              borderColor: "rgba(255, 206, 86, 1)",
+              borderWidth: 1,
+              // data: [43,599,2616]
+              data: no_of_vehicles
+            }
+          ]
+        };
+        
+        var myChart = new Chart(ctx, {
+          type: 'bar',
+          data: severityData,
+          options: {
+            barValueSpacing: 20,
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: true
+                }
+              }]
+            },
+            title: {
+              display: true,
+              text: 'Statistics by Accident Severity: County='+e.target.getFeatures().getArray()[0].get('name')
+            }
+          }
+        });
+      }
+    })
+    .catch(error => {
+      $("#spinner").prop('hidden', true);
+      flash(error,{
+        'bgColor': "#E62229"
+      });    
+    });      
+      
+  }  
 });
